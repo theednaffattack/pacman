82abe41c7d98a1f5491cd07f13b35c0ee72f41a0
Eddie Naff <eddie@eddienaff.dev>
2022-06-02 19:10:08 -0700

    Add canvas element

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-import './style.css'
+export const canvas = document.querySelector<HTMLCanvasElement>("canvas")!;
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
++console.log(canvas);
