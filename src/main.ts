41823208b1a97b5385b33584e39422ec1dd93459
Eddie Naff <eddie@eddienaff.dev>
2022-06-02 21:21:37 -0700

    Add player to game map

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Boundary } from "./boundary";
+import { Player } from "./player";
 
 export const canvas = document.querySelector<HTMLCanvasElement>("canvas")!;
 
@@ -17,6 +18,11 @@ const gameMap = [
   ["-", "-", "-", "-", "-", "-", "-", "-"],
 ];
 if (context) {
+  // Fill the entire canvas with black BEFORE drawing
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Draw our game map.
   gameMap.map((row, rowIndex) =>
     row.map((cellSymbol, cellIndex) =>
       cellSymbol === "-"
@@ -29,4 +35,15 @@ if (context) {
         : undefined
     )
   );
+
+  const pacman = new Player({
+    position: {
+      x: Boundary.width + Boundary.width / 2,
+      y: Boundary.height + Boundary.height / 2,
+    },
+    velocity: { x: 0, y: 0 },
+  });
+
+  // Add our Player
+  pacman.draw(context);
 }
